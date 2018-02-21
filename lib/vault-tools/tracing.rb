502060dd6510d78264d96f11ba22c8e9ea5aed8f
max beizer <max.beizer@gmail.com>
2018-02-21 10:14:50 -0600

    Return early if not configured

--- lib/vault-tools/tracing.rb
+++ lib/vault-tools/tracing.rb
@@ -1,5 +1,9 @@
 module Vault
   module Tracing
+    def self.configure
+      return unless Vault::Tracing.enabled?
+    end
+
     def self.enabled?
       Config[:zipkin_api_host] && Config[:zipkin_enabled] == 'true'
     end
--- test/tracing_test.rb
+++ test/tracing_test.rb
@@ -1,6 +1,10 @@
 require 'helper'
 
 class TracingTest < Vault::TestCase
+  def test_configure_not_enabled
+    refute Vault::Tracing.configure
+  end
+
   def test_enabled_true
     set_env('ZIPKIN_API_HOST', 'https://test-zipin.heroku.tools')
     set_env('ZIPKIN_ENABLED', 'true')
